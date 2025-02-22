a66f3f69b969b40222ac829ec8ea35bed86519a5
ryan deguia <ryandeguia333@gmail.com>
2025-02-22 21:48:12 +0800

    working on pos

--- Frontend/public/js/pos.js
+++ Frontend/public/js/pos.js
@@ -3,10 +3,13 @@
 document.addEventListener("click", function (event) {
   const link = event.target.closest(".nav-link");
   if (!link) return;
+  const heading = document.getElementById("header");
 
   event.preventDefault();
   const page = link.getAttribute("data-page");
-
+  let result = page.replace(".html", "");
+  console.log(result);
+  heading.textContent = result;
   fetch(page)
     .then((response) => response.text())
     .then((data) => {
@@ -14,3 +17,5 @@ document.addEventListener("click", function (event) {
     })
     .catch((error) => console.error("Error loading page:", error));
 });
+
+console.log("hello pos");

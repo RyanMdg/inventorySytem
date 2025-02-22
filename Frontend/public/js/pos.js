c2ceed4ec6bd0a1d79a7db260653159c562fcb58
ryan deguia <ryandeguia333@gmail.com>
2025-02-22 22:37:44 +0800

    working on pos

--- Frontend/public/js/pos.js
+++ Frontend/public/js/pos.js
@@ -18,4 +18,27 @@ document.addEventListener("click", function (event) {
     .catch((error) => console.error("Error loading page:", error));
 });
 
+const togo = document.getElementById("togo");
+const del = document.getElementById("del");
+const cash = document.getElementById("cash");
+const gcash = document.getElementById("gcash");
+const bank = document.getElementById("bank");
+
+function toggleButtons(active, inactive) {
+  active.classList.add("bg-[#B60205]", "text-white");
+  active.classList.remove("bg-[#EAEEF7]");
+
+  inactive.classList.add("bg-[#EAEEF7]");
+  inactive.classList.remove("bg-[#B60205]", "text-white");
+}
+
+togo.addEventListener("click", () => toggleButtons(togo, del));
+del.addEventListener("click", () => toggleButtons(del, togo));
+cash.addEventListener("click", () => toggleButtons(cash, gcash));
+cash.addEventListener("click", () => toggleButtons(cash, bank));
+gcash.addEventListener("click", () => toggleButtons(gcash, cash));
+gcash.addEventListener("click", () => toggleButtons(gcash, bank));
+bank.addEventListener("click", () => toggleButtons(bank, gcash));
+bank.addEventListener("click", () => toggleButtons(bank, cash));
+
 console.log("hello pos");

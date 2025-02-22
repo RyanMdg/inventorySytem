1679b5dfb6bdacb7777488b096db65662fd6b6d8
ryan deguia <ryandeguia.dev@gmail.com>
2025-02-22 09:22:22 +0800

    animation in nav are working

--- Frontend/public/js/navigation.js
+++ Frontend/public/js/navigation.js
@@ -6,6 +6,7 @@ const pos = document.getElementById("pos");
 const container = document.getElementById("Container");
 const inventory = document.getElementById("inventory");
 const branch = document.getElementById("Branch");
+const nav_Items = document.querySelectorAll("nav ul li a");
 
 fetch("../pages/dashboard.html")
   .then((response) => response.text()) // Get response as text
@@ -53,4 +54,29 @@ branch.addEventListener("click", function () {
       container.innerHTML = data;
     })
     .catch((error) => console.error("Error loading the file:", error));
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  nav_Items.forEach((item) => {
+    item.addEventListener("click", function () {
+      nav_Items.forEach((link) =>
+        link.classList.remove(
+          "text-[#B60205]",
+          "bg-[#feebec]",
+          "rounded-sm",
+          "px-7",
+          "py-3",
+          "opacity-50"
+        )
+      );
+
+      this.classList.add(
+        "text-[#B60205]",
+        "bg-[#feebec]",
+        "rounded-sm",
+        "px-7",
+        "py-3"
+      );
+    });
+  });
 });
